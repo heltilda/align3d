2f745f2ee56a411ddd492bea6e3f35a5f425c629
heltilda <heltilda@gmail.com>
2019-02-28 12:10:56 -0700

    Add files via upload

--- seriesExpansions/plotSeriesComparison.m
+++ seriesExpansions/plotSeriesComparison.m
@@ -3,11 +3,11 @@ global seriesSamples logNMax clrs
 
     % adjust these parameters
 
-seriesDir = '~/align3d/Results/large100/analysis/';
-sampleNos = 1:4;
+seriesDir = '/Users/brianross/Desktop/align3d/seriesExpansion/Results/large100/analysis/';
+sampleNos = 1:100;
 binSpacing = .25;
 logNMax = 3;
-infoMax = 0.5;
+infoMax = 0.4;
 clrs = [ .2 .2 .5; .5 .2 .2; .5 .2 .2 ];
 
 
@@ -34,6 +34,8 @@ seriesSamples = cell(3, numBins);
 sampleZ0times = nan(2, numSamples);
 sampleTimeBins = nan(2, numSamples);
 doIndividualPlots = (numSamples <= 10);
+numNans = zeros(2, 2);
+numDataPoints = [ 0 0 ];
 
 
     % make the plots
@@ -62,6 +64,9 @@ for sampleCounter = 1:numSamples
                         + binFrac*log10(bothSeries{loopSeries}(upperTimeBin, idxNterms));
             end
         end
+        
+        numNans(:, loopSeries) = numNans(:, loopSeries) + sum(isnan(bothSeries{loopSeries}(:, [ idxZ idxTit ])), 1)';
+        numDataPoints(loopSeries) = numDataPoints(loopSeries) + size(bothSeries{loopSeries}, 1);
     end
     
     
@@ -105,7 +110,7 @@ for sampleCounter = 1:numSamples
             % draw the I/S plot
         
         subplot(2, numSamples, sampleCounter), hold on
-        colormap(cMap)
+        %colormap(cMap)
         
         plot(seriesXs{1}, bothSeries{1}(:, idxI), '-', 'LineWidth', .5, 'Color', clrs(1, :))
         plot(seriesXs{2}, bothSeries{2}(:, idxI), '-', 'LineWidth', .5, 'Color', clrs(2, :))
@@ -125,13 +130,13 @@ for sampleCounter = 1:numSamples
             plotLabels = { 'series 1', 'series 2', 'S1 iterated', 'S2 iterated' };
             legend(plotLabels)
         end
-        caxis([1 numSamples+1])
+        %caxis([1 numSamples+1])
         
         
             % draw the log Z plot
         
         subplot(2, numSamples, sampleCounter+numSamples), hold on
-        colormap(cMap)
+%        colormap(cMap)
         
         plot(seriesXs{1}, bothSeries{1}(:, idxZ), '-', 'LineWidth', .5, 'Color', clrs(1, :))
         plot(seriesXs{2}, bothSeries{2}(:, idxZ), '-', 'LineWidth', .5, 'Color', clrs(2, :))
@@ -144,7 +149,7 @@ for sampleCounter = 1:numSamples
         if sampleCounter == 1
             ylabel('log Z')
         end
-        caxis([1 numSamples+1])
+%        caxis([1 numSamples+1])
         set(gcf, 'Position', [0, 3000, 250*numSamples, 400])
     end
 end
@@ -203,3 +208,6 @@ if ~doIndividualPlots
     hold on, plot([0 logNMax], [0 0], 'k-.');
     ylabel('S - I'), legend([l1 l2]), set(gcf, 'Position', [0, 3000, 250, 600])
 end
+
+disp(['unconverged w fractions (series 1/2):    ', num2str(numNans(1, :)./numDataPoints)])
+disp(['unconverged f fractions (series 1/2):    ', num2str(numNans(2, :)./numDataPoints)])
